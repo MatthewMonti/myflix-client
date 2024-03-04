98415cc1d9ac892c521f4b3c5bbdc212e08e7ba9
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-03-03 21:29:18 -0500

    update

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,37 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([
+  ]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-  useEffect(() => {
-    fetch("https://movies-flex-6e317721b427.herokuapp.com/api/movies")
-      .then((response) => response.json())
-      .then((data) => {
-        const moviesFromApi = data.docs.map((doc) => {
-          return {
-            id: doc.key,
-            Title: doc.Title,
-            Description: doc.Description,
-            Release: doc.Release,
-            Actor: [doc.Actors],
-            Rated: doc.Rated,
-            Rating: doc.Rating,
-            ImagePath: doc.ImagePath,
-            Featured: doc.Featured
-          };
-        });
-
-        setMovies(moviesFromApi);
-      });
-  }, []);
-
   if (selectedMovie) {
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      <MovieView book={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     );
   }
 
@@ -44,7 +23,7 @@ export const MainView = () => {
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
-          book={movie}
+          movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
           }}
