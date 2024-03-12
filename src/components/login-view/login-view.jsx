7647962633b1eb7f1a70b6e638e24d1d5fe99451
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-03-12 15:30:27 -0400

    Verify Login Data

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,48 @@
+import React from "react";
+
 export const LoginView = () => {
-    return (
-      <form>
-        <label>
-          Username:
-          <input type="text" />
-        </label>
-        <label>
-          Password:
-          <input type="password" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    );
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+const [username, setUsername] = useState("");
+
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://movies-flex-6e317721b427.herokuapp.com/api/user/login", {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (response.ok) {
+        onLoggedIn(username);
+      } else {
+        alert("Login failed");
+      }
+    });
   };
+
+  <input
+  type="text"
+  value={username}
+  onChange={(e) => setUsername(e.target.value)}
+/>
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" />
+      </label>
+      <label>
+        Password:
+        <input type="password" />
+      </label>
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
