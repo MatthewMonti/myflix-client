38062ce5846cdfaf1150028a054652bc64415e6b
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-03-17 16:11:05 -0400

    Apple Look

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,13 @@
 // Here you import the PropTypes library
 import PropTypes from "prop-types";
 
-import { Button, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 // The MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <Card onClick={() => onMovieClick(movie)}>
     <Card.Img className="movie-poster" variant="top" src={movie.Image} />
-    <Card.Body>
-      <Card.Title>{movie.Title}</Card.Title>
-      <Card.Text>{movie.Director.Name}</Card.Text>
-    </Card.Body>
     </Card>
   );
 };
