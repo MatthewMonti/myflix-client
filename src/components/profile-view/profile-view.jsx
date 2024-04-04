cbaedb042194231ec659620a1600891d9159d523
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-04-04 16:58:30 -0400

    display user information

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -4,59 +4,84 @@ import Button from "react-bootstrap/Button"
 
 
 
-export const UserProfile = () => {
-  const [userInfo, setUserInfo] = useState({
-    username: '',
-    birthday: '',
-    email: '',
-    favoriteMovies: []
-  });
+export const UserProfile = ({users}) => {
+    const { userId } = useParams();
+  
+    const user = users.find((u) => u._id === userId);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setUserInfo({ ...userInfo, [name]: value });
-  };
+    if (!user) {
+      return <div>Movie not found</div>;
+  }
+  
 
-  const handleAddFavoriteMovie = () => {
-    const newMovie = prompt('Enter your favorite movie:');
-    if (newMovie) {
-      setUserInfo({
-        ...userInfo,
-        favoriteMovies: [...userInfo.favoriteMovies, newMovie]
-      });
-    }
-  };
-
-  const handleDeleteFavoriteMovie = (index) => {
-    const updatedMovies = [...userInfo.favoriteMovies];
-    updatedMovies.splice(index, 1);
-    setUserInfo({ ...userInfo, favoriteMovies: updatedMovies });
-  };
 
   return (
-    <div>
-      <h2>User Profile</h2>
-      <Form>
-        <Form.Label>Username:
-          <Form.Control type="text" name="username" value={userInfo.username} onChange={handleInputChange} />
-        </Form.Label><br />
-        <Form.Label>Birthday:
-          <Form.Control type="text" name="birthday" value={userInfo.birthday} onChange={handleInputChange} />
-        </Form.Label><br />
-        <Form.Label>Email:
-          <Form.Control type="email" name="email" value={userInfo.email} onChange={handleInputChange} />
-        </Form.Label><br />
-        <Button className="fav">Update</Button>
-      </Form>
-      
-      <h3>Favorite Movies:</h3>
-      <Form>
-      <Form.Label>Favorite Film:
-        <Form.Control type="text" name="text" value={userInfo.email} onChange={handleInputChange} />
-          <Button className="fav">Add:</Button> 
-        <Button className="fav">Delete</Button>
-      </Form.Label>
-      </Form>
-    </div>
+    <Row className="mx-5">
+      <Link to={`/api/users/:userId`}>
+        <img varient="link" className="movie-poster2"src={movie.Image} />
+      </Link> 
+      <br></br>
+      <div>
+        <ReactPlayer className="Video" url={movie.url} /> 
+      </div>  
+      <div>
+        <br></br>
+        <h2>{movie.Title}</h2>
+      </div>
+      <div>
+        <h3 className="Label">Description: </h3>
+        <p>{movie.Description}</p>
+      </div>
+      <div>
+        <h3 className="Label">Release: </h3>
+        <p>{movie.Release}</p>
+      </div>
+      <div>
+        <h3 className="Label">Genre: </h3>
+          <div>
+          <h5 className="Label">Name:</h5>
+          <p className="NO">{movie.Genre.Name}</p>
+          </div>
+          <div>
+          <h5 className="Label">Description: </h5>
+          <p className="NO">{movie.Genre.Description}</p>
+          </div>
+     </div>
+      <div>
+        <h3 className="Label">Rated: </h3>
+        <span>{movie.Rated}</span>
+      </div>
+      <div>
+        <h3 className="Label">Rating: </h3>
+        <p>{movie.Rating}</p>
+      </div>
+      <div>
+       <h3 className="Label">Actors: </h3>
+      <p>{[movie.Actors.join(', ')]}</p>
+      </div>
+      <div>
+        <h3 className="Label">Director: </h3>
+          <div>
+          <h5 className="Label">Name:</h5>
+          <p className="NO">{movie.Director.Name}</p>
+          </div>
+          <div>
+            <h5 className="Label">Bio: </h5>
+            <p className="NO">{movie.Director.Bio}</p>
+          </div>
+          <div>
+            <h5 className="Label">Birth: </h5>
+            <p className="NO">{movie.Director.Birth}</p>
+          </div>
+          <div>
+            <h5 className="Label">Death: </h5>
+            <p className="NO">{movie.Director.Death}</p>
+          </div>
+      </div>    
+      <div>
+        <h5 className="Label">Featured: </h5>
+        <p>{movie.Featured}</p>
+      </div>
+    </Row>
   );
 };
