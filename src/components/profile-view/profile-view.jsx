ab5a33db27f61c255dc48ac1e9d00078a328ff8d
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-04-24 18:31:35 -0400

    update user information

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,9 +1,51 @@
 // UserInfoComponent.js
 
 import React, { useState, useEffect } from 'react';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import CSS from 'bootstrap/dist/css/bootstrap.min.css';
 
 export const UserInfoComponent = () => {
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [Email, setEmail] = useState("");
+  const [showEmail, setShowEmail] = useState("")
+  const [Birthday, setBirthday] = useState("");
+  const [showBirthday, setShowBirthday] = useState("")
   const [userInfo, setUserInfo] = useState(null);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: Username,
+      Password: Password,
+      Email: Email,
+      Birthday: Birthday
+    };
+
+    fetch("https://movies-flex-6e317721b427.herokuapp.com/api/user", {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }
+    }).then((response) => {
+      if (response.ok) {
+        alert("Update user successful");
+        window.location.reload();
+      } else {
+        alert("Update user failed");
+      }
+    });
+  };
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -39,10 +81,86 @@ export const UserInfoComponent = () => {
           <p></p>
         </div>
       )}
+      <Form 
+      onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            className="input-bg"
+            type="text"
+            value={Username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="3" 
+          />
+        </Form.Group>
+  
+        <Form.Group>
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+           type={
+            showPassword ? "text" : "password"
+           }
+            value={Password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <label>Show Password</label>
+            <input
+                type="checkbox"
+                value={showPassword}
+                onChange={() =>
+                    setShowPassword((prev) => !prev)
+                }
+            />
+        </Form.Group>
+  
+        <Form.Group>
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+           type={
+            showEmail ? "text" : "password"
+           }
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <label>Show Email</label>
+            <input
+                type="checkbox"
+                value={showEmail}
+                onChange={() =>
+                    setShowEmail((prev) => !prev)
+                }
+            />
+        </Form.Group>
+  
+        <Form.Group>
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+           type={
+            showBirthday ? "date" : "password"
+           }
+            value={Birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+          <label>Show Birthday</label>
+            <input
+                type="checkbox"
+                value={showBirthday}
+                onChange={() =>
+                    setShowBirthday((prev) => !prev)
+                }
+            />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Update
+        </Button>
+        <br />
+          <br />
+      </Form>
     </div>
   );
 };
-
-
-
-              
+    
