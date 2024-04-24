59f91a85a30d2508453d4b6fed5d5cbfac33a000
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-04-24 16:10:26 -0400

    reset

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,17 @@
-import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
-import Container from "react-bootstrap/Container"
-import "./index.scss";
+import Container from "react-bootstrap/Container";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
 
 const App = () => {
-    return (
-        <Container className="film-info">
-          <MainView />
-        </Container>
-      );
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
 root.render(<App />);
-
