c02029115f0b1db230ab0f058ff1d5a37291adbf
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-05-30 19:58:09 -0400

    altl image text added

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -110,7 +110,7 @@ console.log(movies)
             element={
               <>
               <h3>Welcome to Reel Cinema Database</h3>
-              <img className="col-md-5" src="https://cdn.dribbble.com/users/1913706/screenshots/4353135/reel-alwin.gif"/>
+              <img className="col-md-5" src="https://cdn.dribbble.com/users/1913706/screenshots/4353135/reel-alwin.gif" alt="old projector gif file projector is running"/>
 
               <h4>Login</h4>
                 {user ? (
