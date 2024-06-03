036af550688938d528dc8bfe1375a9adad688bd5
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-06-03 16:54:09 -0400

    style changes to login rounded lights

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -109,7 +109,7 @@ console.log(movies)
             path="/"
             element={
               <Col md={5}>
-              <h3>Welcome to Reel Cinema Database</h3>
+              <h3 id="intro">Welcome to Reel Cinema Database</h3>
               <img className="col-md-12" src="https://cdn.dribbble.com/users/1913706/screenshots/4353135/reel-alwin.gif" alt="gif file old projector is running"/>
 
               <h4>Login</h4>
