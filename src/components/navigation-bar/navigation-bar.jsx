7b80b3ccf39ffd2637619427ca5e8a0791f38622
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-07-22 20:24:08 -0400

    index.html default

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -12,7 +12,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
           <Nav className="me-auto">
             {!user && (
               <>
-                <Nav.Link as={Link} to="/">
+                <Nav.Link as={Link} to="/index.html">
                   Login
                 </Nav.Link>
                 <Nav.Link as={Link} to="/signup">
