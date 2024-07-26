5f9ea5fee11fa6514db0edd6cc9cab33a0e0c12e
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-07-26 17:27:19 -0400

    reset up signup-view

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,118 +1,122 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import CSS from 'bootstrap/dist/css/bootstrap.min.css';
 
 export const SignupView = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [email, setEmail] = useState("");
-  const [showEmail, setShowEmail] = useState(false);
-  const [birthday, setBirthday] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const data = {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday,
+    const [Username, setUsername] = useState("");
+    const [Password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [Email, setEmail] = useState("");
+    const [showEmail, setShowEmail] = useState("")
+    const [Birthday, setBirthday] = useState("");
+    const [showBirthday, setShowBirthday] = useState("")
+  
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        const data = {
+          Username: Username,
+          Password: Password,
+          Email: Email,
+          Birthday: Birthday,
+        };
+    
+        fetch("https://movies-flex-6e317721b427.herokuapp.com/api/create", {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then((response) => {
+          if (response.ok) {
+            alert("Signup successful");
+            window.location.reload();
+          } else {
+            alert("Signup failed");
+          }
+        });
+      };
+  
+      return (
+        <Form 
+        onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Username:</Form.Label>
+            <Form.Control
+              className="input-bg"
+              type="text"
+              value={Username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              minLength="3" 
+            />
+          </Form.Group>
+    
+          <Form.Group>
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+             type={
+              showPassword ? "text" : "password"
+             }
+              value={Password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label>Show Password</label>
+              <input
+                  type="checkbox"
+                  value={showPassword}
+                  onChange={() =>
+                      setShowPassword((prev) => !prev)
+                  }
+              />
+          </Form.Group>
+    
+          <Form.Group>
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+             type={
+              showEmail ? "text" : "password"
+             }
+              value={Email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label>Show Email</label>
+              <input
+                  type="checkbox"
+                  value={showEmail}
+                  onChange={() =>
+                      setShowEmail((prev) => !prev)
+                  }
+              />
+          </Form.Group>
+    
+          <Form.Group>
+            <Form.Label>Birthday:</Form.Label>
+            <Form.Control
+             type={
+              showBirthday ? "date" : "password"
+             }
+              value={Birthday}
+              onChange={(e) => setBirthday(e.target.value)}
+              required
+            />
+            <label>Show Birthday</label>
+              <input
+                  type="checkbox"
+                  value={showBirthday}
+                  onChange={() =>
+                      setShowBirthday((prev) => !prev)
+                  }
+              />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Create Account
+          </Button>
+          <br />
+            <br />
+        </Form>
+      );
     };
-
-    fetch("https://movies-flex-6e317721b427.herokuapp.com/create", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          alert("Signup successful!"); // Optional alert for success
-          window.location.href = '/'; // Redirect to homepage or another route
-        } else {
-          alert("Signup failed");
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("Signup failed");
-      });
-  };
-
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group>
-        <Form.Label>Username:</Form.Label>
-        <Form.Control
-          className="input-bg"
-          type="text"
-          placeholder="Enter your username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          minLength={5}
-          required
-        />
-      </Form.Group>
-
-      <Form.Group>
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-          placeholder="EmpireStar#384"
-           type={
-            showPassword ? "text" : "password"
-           }
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <label>Show Password</label>
-            <input
-                type="checkbox"
-                value={showPassword}
-                onChange={() =>
-                    setShowPassword((prev) => !prev)
-                }
-            />
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Email:</Form.Label>
-          <Form.Control
-            placeholder="stevenson@gmail.com"
-           type={
-            showEmail ? "text" : "password"
-           }
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <label>Show Email</label>
-            <input
-                type="checkbox"
-                value={showEmail}
-                onChange={() =>
-                    setShowEmail((prev) => !prev)
-                }
-            />
-        </Form.Group>
-
-      <Form.Group>
-        <Form.Label>Birthday:</Form.Label>
-        <Form.Control
-          type="date"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Create Account
-      </Button>
-    </Form>
-  );
-};
-
-export default SignupView;
