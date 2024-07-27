bc7fd6530b0108ea0d8917c508ae58b15fa51cc3
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2024-07-26 20:58:27 -0400

    fix button placement

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -107,7 +107,7 @@ export const SignupView = () => {
           required
         />
       </Form.Group>
-
+      <br />
       <Button variant="primary" type="submit">
         Create Account
       </Button>
